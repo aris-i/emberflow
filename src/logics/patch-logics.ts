2e195457d1a43c7c799ef5e14c2208e5304e37de
Jp Venadas <89248840+JPvenadas@users.noreply.github.com>
2025-11-08 15:41:50 +0800

    Add @dataVersion to patched documents, if array-map and map, it will still add @dataVersion in the root document

--- src/logics/patch-logics.ts
+++ src/logics/patch-logics.ts
@@ -136,7 +136,6 @@ export const runPatchLogics = async (appVersion: string, dstPath: string): Promi
       }
 
       console.info(`Running Patch Logics for version ${patchVersion}`);
-      console.debug("Original Document", txnDocument);
       for (const patchLogicConfig of patchLogicConfigs) {
         console.info("Running Patch Logic:", patchLogicConfig.name,);
         const patchLogicStartTime = performance.now();
@@ -146,14 +145,30 @@ export const runPatchLogics = async (appVersion: string, dstPath: string): Promi
         logicResults.push({...patchLogicResult, execTime});
       }
 
-      logicResults.forEach((logicResult) => {
+      for (const logicResult of logicResults) {
         logicResult.transactional = true;
-        logicResult.documents.forEach((document) => {
+        const documentsSnapshot = [...logicResult.documents];
+        for (const document of documentsSnapshot) {
+          const logicResultDocPath = document.dstPath;
+
           if (["merge", "create"].includes(document.action) && document.doc) {
-            document.doc["@dataVersion"] = patchVersion;
+            if (logicResultDocPath.includes("#")) {
+              const rootPath = logicResultDocPath.split("#")[0];
+              logicResult.documents.push({
+                action: "merge",
+                dstPath: rootPath,
+                doc: {"@dataVersion": patchVersion},
+              });
+            } else {
+              document.doc["@dataVersion"] = patchVersion;
+            }
           }
+        }
+        logicResult.documents.push({
+          action: "merge", dstPath,
+          doc: {"@dataVersion": patchVersion},
         });
-      });
+      }
 
       const distributedLogicDocs = await distributeFnTransactional(txn, logicResults);
 
