c66b2964cb55f266d4659e6c77dec43b2719cf97
aris-i <aristotle.isais@gmail.com>
2026-07-10 20:52:13 +0800

    feat(config): bump version to 1.5.0 and extend `ProjectConfig` options
    - Update `package.json` version to 1.5.0.
    feat(core): add support for Firebase Secrets and custom sample initialization
    
    - Enhance `ProjectConfig` to include `secrets` for managing Firebase Secrets, enabling secure API key binding.
    - Add a sample custom project structure in `src/sample-custom` showcasing initialization and configuration of EmberFlow.
    - Update `onUserRegister` logic to support `secrets` in `functionsConfig`, improving flexibility in runtime settings.

--- src/sample-custom/project-config.ts
+++ src/sample-custom/project-config.ts
@@ -1,7 +1,10 @@
-import { ProjectConfig } from "../types";
+import {ProjectConfig} from "../types";
 
 export const projectConfig: ProjectConfig = {
   projectId: "your-project-id",
   region: "us-central1",
+  rtdbName: "your-rtdb-name",
   budgetAlertTopicName: "budget-alerts",
+  maxCostLimitPerFunction: 10,
+  specialCostLimitPerFunction: {},
 };
